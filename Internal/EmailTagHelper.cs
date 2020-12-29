f27cd13f8728d43da92f227ea163cd7ccefdd929
豆奶 <5119871+mrsoymilk@user.noreply.gitee.com>
2020-12-29 22:48:34 +0800

    add self tag helper: email

---
+++ Internal/EmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilkMilk.Internal
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string MailTo { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // base.Process(context, output);
+            output.TagName = "a";
+            var address = "codermrsoymilk@gmail.com";
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+}
---
+++ Internal/MarkdownTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilkMilk.Internal
+{
+    public class MarkdownTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+        }
+    }
+}
